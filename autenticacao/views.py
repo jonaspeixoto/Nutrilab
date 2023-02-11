3e802da8d1d44a85a45bbafb43ba816eaa3ce99c
jonas <jonas.peixoto.087@gmail.com>
2023-02-11 16:49:43 -0300

    Adicionando caminhos estaticos e iniciando desenvolvimento de formularios

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return render(request, 'cadastro.html' )
+
+    if request.method == "GET":
+        return render(request, 'cadastro.html' )
+
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        return HttpResponse(confirmar_senha)
+
 
 def logar(request):
     return HttpResponse('voce está na pagina de login')
