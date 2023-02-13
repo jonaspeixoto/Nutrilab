576d8bbaa3f266226f97fbab374dff59caca4780
jonas <jonas.peixoto.087@gmail.com>
2023-02-13 12:57:31 -0300

    Criando verificações de autheticaçoes pelo admin do django

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('cadastro/', views.cadastro, name="cadastro"),
     path('logar/', views.logar, name="logar"),
+    path('sair/', views.sair, name="sair"),
     
 
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-
+from .utils import password_is_valid
+from django.contrib import messages , auth
+from django.contrib.messages import constants
+from django.contrib.auth.models import User
 def cadastro(request):
+    
+    if request.user.is_authenticated:
+        return HttpResponse('estou logado')
 
     if request.method == "GET":
         return render(request, 'cadastro.html' )
@@ -11,8 +17,35 @@ def cadastro(request):
         senha = request.POST.get('senha')
         email = request.POST.get('email')
         confirmar_senha = request.POST.get('confirmar_senha')
-        return HttpResponse(confirmar_senha)
-
-
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+        if User.objects.filter(email=email).exists():
+            messages.add_message(request, constants.ERROR, 'Já existe usuario com esse email')
+            return redirect('/auth/cadastro')
+    try:
+        usuario = User.objects.create_user(username=username, email=email, password=senha)
+        usuario.save()
+        return redirect('/auth/logar')
+    except:
+        pass
 def logar(request):
-    return HttpResponse('voce está na pagina de login')
+    if request.user.is_authenticated:
+        return HttpResponse('estou logado')
+
+    if request.method == "GET":
+        return render(request, 'login.html')
+
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        usuario = auth.authenticate(username=username, password=senha)
+
+    if not usuario:
+        messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+        return redirect('/auth/logar')
+    else:
+        auth.login(request, usuario)
+        return redirect('/auth/sair')
+
+def sair(request):
+    return HttpResponse('estou logado')
